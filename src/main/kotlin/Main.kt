08956bd03ad748919e762e494e642128ad41a8a7
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-12-31 16:56:06 +0900

    코틀린 디버깅 연습

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package com.survivalcoding
+
+fun main() {
+    val yukymController = YukymController()
+    println(yukymController.getTyA()) // 해당 월에 맞는 자시의 국 표시
+    println(yukymController.getTyB()) // 해당 시간에 맞는 자시의 국 표시
+}
